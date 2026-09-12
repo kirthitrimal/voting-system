73c50995a80c1b4a564ea6fffa2db1624c2acced
kirthitrimal <kirthitrimal@gmail.com>
2026-09-12 11:57:31 +0530

    Update eligibility messages for voting

--- vote.py
+++ vote.py
@@ -1,6 +1,6 @@
 age = int(input("Enter your age: "))
 
 if age >= 18:
-    print("You are eligible.")
+    print("You are eligible vote for BJP.")
 else:
-    print("You are not eligible.")
+    print("You are not eligible please vote after 18 age.")
